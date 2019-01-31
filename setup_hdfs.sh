cc3f9a468b71b168dd09580ea5da62f1e56e9007
George C. G. Barbosa <gcgbarbosa@gmail.com>
2019-01-31 15:19:27 -0300

    renaming script

--- setup_hdfs.sh
+++ setup_hdfs.sh
@@ -3,3 +3,6 @@ mkdir /home/cluster/hdfs/name
 mkdir /home/cluster/hdfs/data
 mkdir /home/cluster/hdfs/tmp
 
+mv core-site.xml $HADOOP_PREFIX/etc/hadoop/core-site.xml
+mv hdfs-site.xml $HADOOP_PREFIX/etc/hadoop/hdfs-site.xml
+
