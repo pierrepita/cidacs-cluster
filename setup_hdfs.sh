254629f8c1021da6c578ca49f55a407c0d55e812
George C. G. Barbosa <gcgbarbosa@gmail.com>
2019-02-04 15:01:25 -0300

    fix bug

--- setup_hdfs.sh
+++ setup_hdfs.sh
@@ -3,6 +3,6 @@ mkdir /home/cluster/hdfs/name
 mkdir /home/cluster/hdfs/data
 mkdir /home/cluster/hdfs/tmp
 
-mv core-site.xml $HADOOP_PREFIX/etc/hadoop/core-site.xml
-mv hdfs-site.xml $HADOOP_PREFIX/etc/hadoop/hdfs-site.xml
+cp core-site.xml $HADOOP_PREFIX/etc/hadoop/core-site.xml
+cp hdfs-site.xml $HADOOP_PREFIX/etc/hadoop/hdfs-site.xml
 
