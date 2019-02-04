8423ed9dedc86e85165e317487240317ea8fddb8
gcgbarbosa <gcgbarbosa@gmail.com>
2019-02-04 15:23:15 -0300

    fixing scripts to work with cluster user

--- setup_hdfs.sh
+++ setup_hdfs.sh
@@ -3,6 +3,6 @@ mkdir /home/cluster/hdfs/name
 mkdir /home/cluster/hdfs/data
 mkdir /home/cluster/hdfs/tmp
 
-cp core-site.xml $HADOOP_PREFIX/etc/hadoop/core-site.xml
-cp hdfs-site.xml $HADOOP_PREFIX/etc/hadoop/hdfs-site.xml
+sudo cp core-site.xml $HADOOP_PREFIX/etc/hadoop/core-site.xml
+sudo cp hdfs-site.xml $HADOOP_PREFIX/etc/hadoop/hdfs-site.xml
 
