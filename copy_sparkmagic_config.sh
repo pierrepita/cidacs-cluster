9acbfe47dd739fedb6772470f0572b42727c69fe
gcgbarbosa <gcgbarbosa@gmail.com>
2019-02-28 10:04:36 -0300

    fixed script to copy livy config

--- copy_sparkmagic_config.sh
+++ copy_sparkmagic_config.sh
@@ -5,9 +5,10 @@ echo ${USERS[@]}
 # for each user
 for i in ${USERS[@]}; do
     # create a folder
+    sudo mkdir /home/$i/.sparkmagic
     # copy file
-    echo user: $i
+    sudo sh -c "cat config.json | sed -e 's/<user>/'$i'/g' > /home/$i/.sparkmagic/config.json"
 done
 
 
-cat config.json | sed -e 's/<user>/george/g'
+#cat config.json | sed -e 's/<user>/george/g'
